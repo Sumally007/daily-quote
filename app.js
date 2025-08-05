21d6f6f6977fdf622872b8d540cc75c9eae3806b
iamismail <ismailmbarouk@gmail.com>
2025-08-05 10:47:46 +0300

    the daily quote app using API is done

---
+++ app.js
@@ -0,0 +1,20 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/random";
+
+async function getQoute(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+
+getQoute(api_url);
+
+function tweet() {
+  const quoteText = encodeURIComponent(quote.innerHTML);
+  const authorText = encodeURIComponent(author.innerHTML);
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText} — by ${authorText}`;
+
+  window.open(tweetUrl, "Tweet Window", "width=600, height=300");
+}
